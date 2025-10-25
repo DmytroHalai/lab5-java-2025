bb43f7ba40cbe4ec79b802a9495beafc5d9a9a12
DmytroHalai <dimagalai04@gmail.com>
2025-10-25 18:09:32 +0300

    Implement Serializable to Book model

--- src/main/java/org/example/model/Book.java
+++ src/main/java/org/example/model/Book.java
@@ -1,7 +1,21 @@
 package org.example.model;
 
+import java.io.Serializable;
+
 public record Book(String title, String author, int yearPublished, String publisher, int pages,
-                   double price) implements Comparable<Book> {
+                   double price) implements Comparable<Book>, Serializable {
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", yearPublished=" + yearPublished +
+                ", publisher='" + publisher + '\'' +
+                ", pages=" + pages +
+                ", price=" + price +
+                '}';
+    }
+
     @Override
     public int compareTo(Book book) {
         return this.publisher().compareToIgnoreCase(book.publisher());
