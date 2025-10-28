07cb532ebd5e414a826a2bc9c20c77816f62ca90
DmytroHalai <dimagalai04@gmail.com>
2025-10-28 23:35:56 +0200

    Extract similar logic of encryption to the private method

--- src/main/java/org/example/utils/CryptoService.java
+++ src/main/java/org/example/utils/CryptoService.java
@@ -4,41 +4,22 @@ import java.io.*;
 
 public class CryptoService {
     public static void encrypt(String inputFile, String outputFile, char key) {
-        try (FileReader fr = new FileReader(inputFile);
-             FilterWriter fw = new FilterWriter(new FileWriter(outputFile)) {
-                 @Override
-                 public void write(int c) throws IOException {
-                     super.write(c + key);
-                 }
-             }) {
-            int c;
-            while ((c = fr.read()) != -1) {
-                fw.write(c);
-            }
-        } catch (FileNotFoundException fnf) {
-            System.err.println("Input file not found");
-
-        } catch (SecurityException sec) {
-            System.err.println("Access denied when reading/writing files. Check file permissions.");
-
-        } catch (IOException io) {
-            System.err.println("I/O error while processing files: " + io.getMessage());
-
-        } catch (IllegalArgumentException ex) {
-            System.err.println("Invalid encryption key or parameters: " + ex.getMessage());
-
-        } catch (Exception e) {
-            System.err.println("Unexpected error: " + e.getClass().getSimpleName() + " — " + e.getMessage());
-        }
-
+        boolean doDecrypt = false;
+        engine(inputFile, outputFile, key, doDecrypt);
     }
 
     public static void decrypt(String inputFile, String outputFile, char key) {
+        boolean doDecrypt = true;
+        engine(inputFile, outputFile, key, doDecrypt);
+    }
+
+    private static void engine(String inputFile, String outputFile, char key, boolean doDecrypt) {
         try (FileReader fr = new FileReader(inputFile);
              FilterWriter fw = new FilterWriter(new FileWriter(outputFile)) {
                  @Override
                  public void write(int c) throws IOException {
-                     super.write(c - key);
+                     if (doDecrypt) super.write(c - key);
+                     else super.write(c + key);
                  }
              }) {
 
